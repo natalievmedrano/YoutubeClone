393ee7c505558e104fc2e0e6b1f60a7527afc4ac
natalievmedrano <nataliemed0403@gmail.com>
2023-02-16 10:47:33 -0600

    created get,post functions for replies app + updated urls. Need to work on getting post function to work

--- backend/replies/urls.py
+++ backend/replies/urls.py
@@ -1,6 +1,13 @@
 from django.urls import path, include
 from replies import views
 
+from django.contrib import admin
+from django.urls import path, include
+
 urlpatterns = [
-    path('all/', views.get_all_replies)
+    path('admin/', admin.site.urls),
+    path('api/auth/', include('authentication.urls')),
+    path('', views.user_replies),
+    path('all/', views.get_all_replies),
+
 ]
