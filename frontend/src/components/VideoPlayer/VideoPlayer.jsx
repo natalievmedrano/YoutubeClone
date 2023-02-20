dbf0c81c5f6cc4eb9f1329934cf5a626119afe1e
natalievmedrano <nataliemed0403@gmail.com>
2023-02-20 17:00:06 -0600

    changed description font size and added title and description class names

--- frontend/src/components/VideoPlayer/VideoPlayer.jsx
+++ frontend/src/components/VideoPlayer/VideoPlayer.jsx
@@ -1,12 +1,15 @@
 import React from "react";
+import './VideoPlayer.css'
 
 const VideoPlayer = ({ video, videoId }) => {
   if (video) {
     return (
       <div>
         <iframe src={`https://www.youtube.com/embed/${videoId}`}></iframe>
+        <h2>TITLE</h2>
         <h4>{video.snippet.title}</h4>
-        <p>{video.snippet.description}</p>
+        <h2>DESCRIPTION</h2>
+        <p id="description">{video.snippet.description}</p>
       </div>
     );
   }
